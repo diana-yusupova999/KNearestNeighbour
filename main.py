2c61583132b7034658434a1c57ea55838300ae6f
drug-di <53917103+drug-di@users.noreply.github.com>
2022-05-25 14:38:43 +0300

    Initial commit KNN algorithm

---
+++ main.py
@@ -0,0 +1,95 @@
+import math
+import operator
+import csv
+import random
+
+def loadDataset(filename, split, trainingSet=[], testSet=[]):
+    with open(filename, 'r') as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        for x in range(len(dataset)):
+            for y in range(53):
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split:
+                trainingSet.append(dataset[x])
+            else:
+                testSet.append(dataset[x])
+
+"""евклидово расстояние: квадратный корень из суммы квадратов разностей между двумя массивами чисел
+length - длина (сколько данных берем из датасет)"""
+
+
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
+
+
+"""возвращает k наиболее похожих соседей из обучающего набора для данного экземпляра теста, 
+используя определенный euclideanDistance"""
+
+
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance) - 1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+
+"""Мы нашли наиболее похожих соседей для тестового экземпляра. 
+Теперь нужно разработать прогнозируемый ответ на основе этих соседей.
+Мы можем сделать это, позволив каждому соседу проголосовать за свой атрибут класса и принять прогноз большинства."""
+
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+
+"""функция суммирует итоговые правильные прогнозы и возвращает точность в процентах от правильных классификаций"""
+
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+
+def main():
+    # prepare data
+    trainingSet = []
+    testSet = []
+    split = 0.90
+    loadDataset('data.data', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
+    # generate predictions
+    predictions = []
+    k = 3
+    for x in range(len(testSet)):
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
+
+
+if __name__ == '__main__':
+    main()
